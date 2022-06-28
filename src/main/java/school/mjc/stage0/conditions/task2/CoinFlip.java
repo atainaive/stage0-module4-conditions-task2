eefeff1e1dd7814060e610583d05ac41c2ce3d18
atainaive <atai.omorov@stud.vgtu.lt>
2022-06-28 10:22:05 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -4,8 +4,8 @@ public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
         if(from1UpTo1000 < 500){
             System.out.println("Eagle");
-        } else {
-            System.out.println("Tale");
+        } else if(from1UpTo1000 >= 500) {
+            System.out.println("Tail");
         }
     }
 }
